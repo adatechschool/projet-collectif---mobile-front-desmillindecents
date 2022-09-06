7f487f469a8b7d2f85aa096693738d8e8fb08840
Vikelle <virginie.kellermann@gmail.com>
2022-09-06 14:16:58 +0200

    création ProfilList / Data.tsx

--- src/components/ProfilList.tsx
+++ src/components/ProfilList.tsx
@@ -1,10 +1,33 @@
-export const profilList = [
+import { StyleSheet, Text, Image, View } from "react-native";
+import {data} from './Data';
+
+const ProfilList = () => {
+  return(
+    <View>
     {
-        Image: "../../assets/Photo_profil_1.png",
-        Name: "Robert Fox"
-    },
-    {
-        Image: "../../assets/Photo_profil_1.png",
-        Name: "Robert Fox"
-    }
-]
+    data.map((l) => (
+        <View style={ styles.footer }>
+          <Image source={{uri:l.Image}} style={ styles.Image }/>
+          <Text>{l.Name}</Text>
+        </View>)
+    )}
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+    footer :{
+        backgroundColor: '#D9D9D9',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        width: 500,
+        height: 90,
+      },
+      Image: {
+        height:70,
+        width:70,
+      }
+});
+
+export default ProfilList;
