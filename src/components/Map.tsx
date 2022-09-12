d505727fdf78f4331f09749c29d5e695b7ca1c8f
Bynood <quent1_jarry@hotmail.com>
2022-09-12 10:50:33 +0200

    reduce map size(0.75) to see footer

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -36,7 +36,7 @@ const map = () =>{
   const styles = StyleSheet.create({
     map: {
       width: Dimensions.get('window').width,
-      height: Dimensions.get('window').height*0.8,
+      height: Dimensions.get('window').height*0.75,
     },
   });
 
